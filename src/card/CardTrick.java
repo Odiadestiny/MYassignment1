5b51ac8d9d8e6b7f653cf7674da60fa03d09f154
DestinyOdia <odiadestiny56@gmail.com>
2024-01-30 14:16:45 -0500

    Merge commit 'bc7a118f3d59de9243cc0fb8f20b632c00d2ee2b' into Destiny_Odia_ICE1
    Conflicts:
    	src/card/CardTrick.java

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -35,16 +35,13 @@ public class CardTrick {
         luckyCard.setValue(2);
         luckyCard.setSuit(Card.SUITS[3]);
         
-        magicHand[magicHand.length-1] = luckyCard;
         
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
         // add one luckcard hard code 2,clubs
         
-        Card luckyCard = new Card();
-        luckyCard.setValue(2);
-        luckyCard.setSuit(Card.SUITS[3]);
+        
         
         
         boolean check = false;
