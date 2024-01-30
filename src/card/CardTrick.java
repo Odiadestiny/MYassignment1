74cfb67f2e3aef8a3b1d48128b210b7eb67f0236
DestinyOdia <odiadestiny56@gmail.com>
2024-01-30 13:03:28 -0500

    version 2 ICE1

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -30,11 +30,6 @@ public class CardTrick {
             
             magicHand[i] = c;
         }
-        Card luckyCard = new Card();
-        luckyCard.setValue(2);
-        luckyCard.setSuit(Card.SUITS[3]);
-        
-        magicHand[magicHand.length-1] = luckyCard;
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
